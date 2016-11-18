97a78aae86b491ff155856e72dd3fecfe0d3dcc5
Gulnur <gsshamgunova@gmail.com>
2016-11-18 11:11:20 +0300

    commit

---
+++ reflection/BeanUtils.java
@@ -0,0 +1,46 @@
+package com.company;
+
+
+import java.lang.reflect.*;
+import java.util.*;
+
+
+public class BeanUtils {
+    public static void assign(Object from, Object to) throws InvocationTargetException, IllegalAccessException {
+        Class fromClass = from.getClass();
+        Class toClass = to.getClass();
+
+        Map<String, Method> map = new HashMap<>();
+
+        for (Method method : fromClass.getDeclaredMethods()) {
+            String methodName = method.getName().toLowerCase();
+            if (methodName.startsWith("get")) {
+                methodName = methodName.substring(3, methodName.length());
+                map.put(methodName, method);
+            }
+        }
+
+        for (Method method : toClass.getDeclaredMethods()) {
+            String methodName = method.getName().toLowerCase();
+            if (methodName.startsWith("set")) {
+                methodName = methodName.substring(3, methodName.length());
+
+                for (String key : map.keySet()) {
+                    if (key.equals(methodName)) {
+                        if (method.getParameterCount() == 1 && map.get(methodName).getReturnType().equals(method.getParameterTypes()[0])) {
+                            try {
+                                method.invoke(to, map.get(methodName).invoke(from));
+                            } catch (InvocationTargetException e) {
+                                e.printStackTrace();
+                            } catch (IllegalAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
